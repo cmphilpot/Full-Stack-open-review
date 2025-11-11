e8fa77e7abca8aaa13b5d2e130e737927a61d798
Chase Philpot <philpotchase@gmail.com>
2025-11-11 16:26:10 -0500

    Exercise 1.5: refactor course and parts into single object

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,28 +1,30 @@
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [ 
-    {
-      id: 1,
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      id: 2,
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      id: 3,
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        id: 1,
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        id: 2,
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        id: 3,
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-  const Header = ({course}) => {
+  const Header = ({name}) => {
     return (
       <>
-        <h1>{course}</h1>
+        <h1>{name}</h1>
       </>
     )
   }
@@ -66,19 +68,18 @@ const App = () => {
     )
   }
 
-
   return (
     <div>
       <Header 
-        course={course} 
+        name={course.name} 
       />
    
       <Content 
-        parts={parts} 
+        parts={course.parts} 
       />
 
       <Total 
-        parts={parts}
+        parts={course.parts}
       />
     </div>
   )
