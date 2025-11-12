c35d429a3dd86752387158cdb5e97a94faa39ce3
Chase Philpot <philpotchase@gmail.com>
2025-11-12 10:42:17 -0500

    Unicafe: complete step 2; add good, neutral, bad buttons with respective handlers and include statistics for all, average, and positive

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,22 +1,35 @@
 import { useState } from 'react'
 
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const all = good + neutral + bad;
+  const average = all > 0 ? ((good - bad) / all) : 0;
+  const positive = all > 0 ? ((good / all) * 100) : 0;
+
+  const handleGoodClick = () => setGood(good + 1);
+  const handleNeutralClick = () => setNeutral(neutral + 1);
+  const handleBadClick = () => setBad(bad + 1);
 
   return (
     <div>
       <h1>Give Feedback</h1>
-        <button>Good</button>
-        <button>Neutral</button>
-        <button>Bad</button>
+      <Button onClick={handleGoodClick} text='Good' />
+      <Button onClick={handleNeutralClick} text='Neutral'/>
+      <Button onClick={handleBadClick} text='Bad'/>
 
       <h1>Statistics</h1>
-        <p>Good</p>
-        <p>Neutral</p>
-        <p>Bad</p>
+        <p>Good {good}</p>
+        <p>Neutral {neutral}</p>
+        <p>Bad {bad}</p>
+        <p>All {all}</p>
+        <p>Average {average}</p>
+        <p>Positive {positive}</p>
     </div>
   )
 }
